61cb8dae4d053bf8411c71f4a90ba49b4e501627
eggdragon <dragoneggscollections@gmail.com>
2023-01-19 09:30:32 +0900

    workflowtest v.1.1.7

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { RouterConfig } from "./pages/RouterConfig";
 import { LoginProvider } from "./pages/common/LoginContext";
 
 export const App = () => {
-  console.log("workflowtest");
   return (
     <LoginProvider>
       <RouterConfig />
